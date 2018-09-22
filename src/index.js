6639c0d33a5e1e690a5089619e94e2e921d8625d
venomamd <komashh@gmail.com>
2018-09-22 14:46:04 +0300

    task4 solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+ 
+  let arr = [];
+  let solutions = [];
+  let a, b, c, D;
+  let x1, x2;
+  const regexp = /[^x^]\d+/g;
+  
+  equation = equation.replace(/\s/g, "");
+  arr = equation.match(regexp);
+
+  a = parseInt(arr[0]);
+  b = parseInt(arr[1]);
+  c = parseInt(arr[2]);
+  D = b * b - 4 * a * c;
+
+  if(D > 0) {
+    x1 = Math.round((- b + Math.sqrt(D)) / (2 * a));
+    x2 = Math.round((- b - Math.sqrt(D)) / (2 * a));
+    solutions = [x1, x2];
+    return solutions.sort((a, b) => a-b);
+  }else if(D < 0) {
+    solution = [null, null];
+    return solutions;
+  }  else if(D === 0) {
+    x1 = Math.round((- b) / (2 * a));
+    solutions = [x1, null];
+    return solutions;
+  }
+
 }
